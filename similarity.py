fd38b1046812ec668438e7c76969b0b71a65b397
hanjiaxing521 <862339523@qq.com>
2022-01-30 16:04:29 +0800

    add annotation of files

---
+++ similarity.py
@@ -0,0 +1,10 @@
+'''
+description: get neighbours similar with active user.
+
+process: 1. compute similarities between active user and other users.
+         2. choose top n in other users to be neighbours of active user.
+
+date: 2022-01-30 15:47
+
+author:
+'''
