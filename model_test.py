b7c000c82f1257b225aa2534ee7f5136f7b91bb1
hanjiaxing521 <862339523@qq.com>
2022-02-26 00:01:44 +0800

    add model_test

--- model_test.py
+++ model_test.py
@@ -5,5 +5,26 @@ process:
 
 date: 2022-01-30 16:03
 
-author:
+author: Hanjiaxing
 '''
+
+from data_input import load_rating_data
+from svd import svd_matrix
+from recommend import rating_predict, recommend_n_movie
+import copy
+
+
+def main():
+
+    U = load_rating_data()  # 加载用户-电影评分矩阵
+    U_origin = copy.deepcopy(U)  # 拷原始用户-电影评分矩阵用以保留
+
+    UE = svd_matrix(U)  # 对U矩阵进行svd, 默认取前100%奇异值
+
+    U = rating_predict(U, UE)  # 预测评分并填充U矩阵
+
+    recommend_n_movie(U_origin, U)  # 打印TopN推荐电影结果
+
+
+if __name__ == "__main__":
+    main()
